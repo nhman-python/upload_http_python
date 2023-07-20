b631f6611ad4d3411c679f3e496b291eae189119
python user <104791322+nhman-python@users.noreply.github.com>
2023-07-20 03:05:05 +0300

    Update upload_server.py

--- upload_server.py
+++ upload_server.py
@@ -1,8 +1,56 @@
 import os
 import uuid
 from datetime import timedelta
-from flask import render_template, request, Flask, redirect, url_for, session
-from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+from flask import render_template, request, Flask, redirect, url_for, session, flash
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.permanent_session_lifetime = timedelta(days=20)
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20mb
+db = SQLAlchemy(app)
+
+# ----------- config ----------- #
+
+ALLOW_UPLOAD = True
+ALLOW_EXTENSION = ('.txt', '.jpg', '.zip')  # add here an allowed extension
+UPLOAD_FOLDER = 'upload_file'
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    print(f'upload folder created {UPLOAD_FOLDER}')
+
+
+# ----------- end config -------- #
+
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+# Create the database and tables
+if not os.path.exists('users.db'):
+    with app.app_context():
+        db.create_all()
+
+
+def valid_path(upload_path: str) -> bool:
+    """
+    Validates that the path is in the upload folder and not exploitable with ../..
+    or something similar.
+    :param upload_path: The file path to validate.
+    :return: True if the path is valid, False otherwise.
+    """
+    upload_folder_path = os.path.abspath(UPLOAD_FOLDER)
+    full_path = os.path.abspath(os.path.join(upload_folder_path, upload_path))
+    return os.path.commonprefix([upload_folder_path, full_path]) == upload_folder_path
 
 
 def format_size(size):
@@ -19,37 +67,10 @@ def format_size(size):
     return f"{round(size)} {power_labels[n]}"
 
 
-app = Flask(__name__)
-app.secret_key = 'secret_token'
-app.permanent_session_lifetime = timedelta(days=20)
-
-# ----------- config ----------- #
-
-ALLOW_UPLOAD = True
-ALLOW_EXTENSION = ('.txt', '.jpg', '.zip')  # add here an allowed extension
-MAX_SIZE = 20 * 1024 * 1024
-UPLOAD_FOLDER = 'upload_file'
-MAX_SIZE_ROUND = format_size(MAX_SIZE)
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-    print(f'upload folder created {UPLOAD_FOLDER}')
+UPLOAD_ROUND = format_size(app.config['MAX_CONTENT_LENGTH'])
 
 
-# ----------- end config -------- #
-
-def get_password():
-    with open('.env', 'r') as f:
-        password_value = f.readline().split('=')[1].strip()
-    return password_value
-
-
-def get_admin_user():
-    with open('.env', 'r') as f:
-        admin_value = f.readline().split('=')[1].strip()
-    return admin_value
-
-
-def check_file(file_name: str) -> bool:
+def valid_extension(file_name: str) -> bool:
     """
     This function checks if the file is allowed to be uploaded to the server.
     :param file_name: The file path to check the extension
@@ -61,107 +82,158 @@ def check_file(file_name: str) -> bool:
     return False
 
 
-def random_name(file_path):
+def random_file_name(file_path):
+    """
+    add uuid in the start of the given file path to handel duplication in the upload folder
+    :param file_path: the path
+    :return: uuid + path + extension
+    """
     extension = os.path.splitext(file_path)[1]
-    new_path = os.path.join(UPLOAD_FOLDER, f'{uuid.uuid1()}.{extension}')
+    new_path = os.path.join(UPLOAD_FOLDER, f'{uuid.uuid1()}{extension}')
     return new_path
+
+
+# Update the login_required decorator to redirect to the login page
+def login_required(view_func):
+    @wraps(view_func)
+    def wrapped_view(*args, **kwargs):
+        if 'user_id' in session:
+            return view_func(*args, **kwargs)
+        else:
+            flash('You need to log in to access this page.', 'error')
+            return redirect(url_for('login'))
+
+    return wrapped_view
+
+
+@app.errorhandler(413)
+def request_entity_too_large(_):
+    return render_template('index.html',
+                           error='The file size exceeds the limit. Maximum allowed file size is 20 MB.'), 413
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files.get('file')
-        if ALLOW_UPLOAD:
-            if file and check_file(file.filename):
-                if file.content_length <= MAX_SIZE:
-                    random_path = random_name(file.filename)
-                    file.save(random_path)
-                    return render_template('index.html', info='The file has been saved on the server.')
-                else:
-                    return render_template('index.html',
-                                           error=f'File size exceeds the limit. Maximum allowed file size is '
-                                                 f'{MAX_SIZE_ROUND}.',
-                                           ), 403
-            else:
-                return render_template('index.html',
-                                       error=f'Error: No file selected or file extension not allowed. List of '
-                                             f'allowed extensions: {ALLOW_EXTENSION}',
-                                       ), 400
-        return render_template('index.html', error='the admin lock the upload options'), 403
+        file_get = request.files.get('file')
+        if not file_get:
+            return render_template('index.html', error='Error: No file selected.'), 400
+        if not ALLOW_UPLOAD:
+            return render_template('index.html', error='The admin has locked the upload for now please try later.'), 403
+
+        if not valid_extension(file_get.filename):
+            return render_template('index.html',
+                                   error=f'Only this extension are allowed: {", ".join(ALLOW_EXTENSION)}.'), 400
+
+        if file_get.content_length >= app.config['MAX_CONTENT_LENGTH']:
+            return render_template('index.html',
+                                   error=f'File size exceeds the limit. Maximum allowed file size is 20mb.'), 403
+        random_path = random_file_name(file_get.filename)
+        file_get.save(random_path)
+        return render_template('index.html', info='The file has been saved on the server.')
+
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@login_required
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username or not password:
+            flash('Please fill in all the fields', 'error')
+            return redirect(url_for('register'))
+
+        # Check if the username already exists in the database
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different username.', 'error')
+            return redirect(url_for('register'))
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful. You can now login.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/delete-file', methods=['POST'])
+@login_required
+def delete():
+    file_path = request.form.get('file_path')
+    full_path = os.path.abspath(os.path.join(UPLOAD_FOLDER, file_path))
+
+    if not valid_path(full_path):
+        flash('Invalid file path.', 'error')
+    elif os.path.isfile(full_path):
+        os.remove(full_path)
+        flash('File deleted successfully.', 'success')
+    else:
+        flash('File not found.', 'error')
+
+    return redirect(url_for('admin'))
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
+def admin():
+    global ALLOW_UPLOAD
+    if request.method == 'POST':
+        allow_upload = request.form.get('allow_upload')
+        ALLOW_UPLOAD = not ALLOW_UPLOAD if allow_upload else ALLOW_UPLOAD
+    # User is logged in, allow access to the admin page
+    # to Render the admin page
+    list_file = os.listdir(UPLOAD_FOLDER)
+
+    return render_template('admin.html', file_count=len(list_file),
+                           files=list_file, allow_upload=ALLOW_UPLOAD)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    admin_password = get_password()
-    admin_username = get_admin_user()
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if check_password_hash(admin_username, username) and check_password_hash(admin_password, password):
-            session['logged_in'] = True
-        return redirect(url_for('admin'))
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash('Login successful.', 'success')
+            return redirect(url_for('admin'))
+        else:
+            flash('Invalid username or password.', 'error')
+
     return render_template('login.html')
 
 
-@app.route('/logout')
-def logout():
-    # Clear the session and redirect to the login page
-    session.clear()
-    return redirect(url_for('login'))
-
-
-@app.route('/delete-file', methods=['POST'])
-def delete():
-    if 'logged_in' in session:
-        file_path = request.form.get('file_path')
-        full_path = os.path.join(UPLOAD_FOLDER, file_path)
-        if os.path.isfile(full_path):
-            os.remove(full_path)
-        return redirect(url_for('admin'))
-    return redirect('/')
-
-
-@app.route('/admin', methods=['GET', 'POST'])
-def admin():
-    global ALLOW_UPLOAD
-    # Check if the 'logged_in' key exists in the session
-    if 'logged_in' in session:
-        if request.method == 'POST':
-            allow_upload = request.form.get('allow_upload')
-            ALLOW_UPLOAD = not ALLOW_UPLOAD if allow_upload else ALLOW_UPLOAD
-        # User is logged in, allow access to the admin page
-        # to Render the admin page
-        list_file = os.listdir(UPLOAD_FOLDER)
-
-        return render_template('admin.html', file_count=len(list_file),
-                               files=list_file, allow_upload=ALLOW_UPLOAD)
-    # User is not logged in, redirect to the login page
-    return redirect(url_for('login'))
-
-
 @app.route('/change-password', methods=['POST', 'GET'])
+@login_required
 def change_password():
-    if 'logged_in' in session:
-        if request.method == 'POST':
-            change_pass = request.form.get('password')
-            change_admin = request.form.get('username')
-            if change_pass and change_admin:
-                password = generate_password_hash(change_pass)
-                admin_user = generate_password_hash(change_admin)
-                if os.path.isfile('.env'):
-                    with open('.env', 'w') as f:
-                        f.write(
-                            f'PASSWORD={password}\n'
-                            f'ADMIN_USER={admin_user}'
-                        )
-                        session.pop('logged_in')
-                    return render_template('change_password.html', success='the password change successfully',
-                                           redict='True')
+    if request.method == 'POST':
+        new_password = request.form.get('password')
+        if new_password:
+            user = User.query.get(session['user_id'])
+            hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
+            user.password = hashed_password
+            db.session.commit()
+            flash('Password changed successfully.', 'success')
+            return redirect(url_for('admin'))
 
-        return render_template('change_password.html')
-
-    return redirect('/')
+    return render_template('change_password.html')
 
 
-app.run(debug=True)
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('user_id', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
+
+
+app.run(debug=False)
